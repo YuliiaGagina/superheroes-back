802ebb42d27a0e9998e5338eec370ece6b0521e8
YuliiaGagina <ygaginazy@gmail.com>
2023-05-30 00:00:56 +0300

    fix apload images

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const DB_HOST =
-  "mongodb+srv://ygaginazy:Logitech1498791@yuliia.sjlwl4k.mongodb.net/recipes-store";
+  "mongodb+srv://ygaginazy:Logitech1498791@yuliia.sjlwl4k.mongodb.net/superheroes-db";
 
 module.exports = {
   DB_HOST,
--- controllers/superheroes/addAvatar.js
+++ controllers/superheroes/addAvatar.js
@@ -4,11 +4,13 @@ const { Superhero } = require("../../model");
 const { NotFound } = require("http-errors");
 
 const addAvatar = async (req, res) => {
+  // const { path: tempUpload, originalname } = req.file;
   const { id } = req.params;
   const { images } = req.body;
   const files = req.files;
   const fileNames = files.map((file) => file.originalname);
 
+  // const imageName = `${id}_${originalname}`;
   try {
     const uploadPromises = files.map((file) => {
       const imageName = `${file.originalname}`;
@@ -27,16 +29,14 @@ const addAvatar = async (req, res) => {
     const imagesnew = fileNames.map((fileName) =>
       path.join("public", "avatars", `${fileName}`)
     );
+    const hero = await Superhero.findById(id);
+    if (!hero) {
+      throw new NotFound(`Superhero with id ${id} does not found`);
+    }
+    hero.images.push(...imagesnew);
+    await hero.save();
 
-    const newSuperhero = await Superhero.findByIdAndUpdate(
-      id,
-      { ...req.body, images: imagesnew },
-      {
-        new: true,
-      }
-    );
-
-    res.json(newSuperhero);
+    res.json(hero);
   } catch (error) {
     await fs.unlink(tempUpload);
     throw error;
